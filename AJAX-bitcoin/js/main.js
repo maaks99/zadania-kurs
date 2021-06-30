1704425d92dd51303424057cda5807d49a01025d
maaks99 <revn@op.pl>
2021-06-30 19:03:39 +0200

    m

--- AJAX-bitcoin/js/main.js
+++ AJAX-bitcoin/js/main.js
@@ -1,42 +1,82 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    let previousExchange;
-
-    $('#btn').click(function(){
-        
-
-        
-
+    let buyExchange;
+    let sellExchange;
+  
+    $('#btn').click(function () {
+    
+        $.ajaxSetup({ cache: false });
         $.getJSON('https://blockchain.info/pl/ticker')
-        .done(function(data){
-           console.log(previousExchange);
-           
-            let buy = $('#buy');
-            let sell = $('#sell');
-            let up = $('.fa-arrow-up');
-            let down = $('.fa-arrow-down');
-            let buynumber = $('<p></p>').attr('id','buy-number');
-            let sellnumber = $('<p></p>').attr('id','sell-number');
+            .done(function (data) {
+                
+                let buy = $('#buy');
+                let sell = $('#sell');
+                let up = $('.fa-arrow-up');
+                let down = $('.fa-arrow-down');
+                let up2 = $('#up2');
+                let down2 = $('#down2');
+                let equal = $('.linia');
+                let equal2 = $('.linia2');
+                let buynumber = $('<p></p>').attr('id', 'buy-number');
+                let sellnumber = $('<p></p>').attr('id', 'sell-number');
 
-            buynumber.text(`${data["PLN"].buy} `);
-            sellnumber.text(`${data["PLN"].sell} `);
-           
-            buy.append(buynumber);
-            sell.append(sellnumber);
-           
+                buy.empty();
+                sell.empty();
 
-            if(data["PLN"].buy >previousExchange) {
-                //buynumber.append(up);
-                console.log(data["PLN"].buy);
-                console.log(previousExchange);
-            }
+                buynumber.text(`${data["PLN"].buy} `);
+                sellnumber.text(`${data["PLN"].sell} `);
 
-          
-           //sellnumber.append(down);
-           previousExchange = data["PLN"].buy;
-        })
-        .fail(function(error){
-            console.error(error);
-        });
+                buy.append(buynumber);
+                sell.append(sellnumber);
+              
+                
+                if (data["PLN"].buy > buyExchange) {
+                    buynumber.append(up);
+                    up.css("visibility", "visible");
+                    up.css("display", "inline-block")
+
+                } else {
+                    buynumber.append(down);
+                    down.css("visibility", "visible");
+                    down.css("display", "inline-block");
+
+                }
+
+                if (data["PLN"].sell > sellExchange) {
+                    sellnumber.append(up2);
+                    up2.css("visibility", "visible");
+                    up2.css("display", "inline-block");
+
+                } else {
+                    sellnumber.append(down2);
+                    down2.css("visibility", "visible");
+                    down2.css("display", "inline-block");
+
+                }
+
+                if (data["PLN"].buy === buyExchange) {
+                    buynumber.append(equal);
+                    equal.css("visibility", "visible");
+                    up.css("display", "none");
+                    down.css("display", "none");
+
+                }
+
+                if (data["PLN"].sell === sellExchange) {
+                    sellnumber.append(equal2);
+                    equal2.css("visibility", "visible");
+                    down2.css("display", "none");
+                    up2.css("display", "none");
+                }
+
+                buyExchange = data["PLN"].buy;
+                sellExchange = data["PLN"].sell;
+                
+            })
+            .fail(function (error) {
+                console.error(error);
+            });
+        
     });
+
 });
